f6552033df7adddb3ff312bec84c5481a9de1ff9
rharkovskyProf <135274959+rharkovskyProf@users.noreply.github.com>
2025-11-28 11:55:46 -0300

    Add files via upload

--- BibliotecaDig/BibliotecaDigital.java
+++ BibliotecaDig/BibliotecaDigital.java
@@ -10,19 +10,68 @@ public class BibliotecaDigital {
 		
 	}
 	
-	public int contarItensPorTipo() {
-		return(this.acervo.size());
-	}
+//	public int contarItensPorTipo() {
+//		return(this.acervo.size());
+//	}
+	
+	public void contarItensPorTipo() { // 
+        int totalLivros = 0;
+        int totalRevistas = 0;
+
+        for (ItemBiblioteca item : acervo) {
+
+            if (item instanceof Livro) { 
+                totalLivros++;
+            } else if (item instanceof Revista) { 
+                totalRevistas++;
+            }
+        }
+
+        // Imprime o resultado
+        System.out.println("\n--- Contagem de Itens ---"); 
+        System.out.println("Total de Livros: " + totalLivros);
+        System.out.println("Total de Revistas: " + totalRevistas);
+    }
 	
 	public void listarItens() {
 		for (ItemBiblioteca item: acervo) {
 			item.exibirInformacoes();
 		}
+		
+		contarItensPorTipo();
+	}
+	
+	public boolean apagarItem(String titulo) {
+		
+		boolean resultado = false;
+		int cont = 0;
+		
+        for (ItemBiblioteca item : acervo) {
+        	if (item.titulo.contains(titulo)) {
+        		this.acervo.remove(cont);
+        		resultado = true;
+        		break;
+        	}
+        	cont++;
+         }
+        return(resultado);
 	}
 
+	public ArrayList<ItemBiblioteca> buscarPorTitulo(String titulo) {
+		
+		ArrayList<ItemBiblioteca> resultado =  new ArrayList<>();
+		
+        for (ItemBiblioteca item : acervo) {
+        	if (item.titulo.contains(titulo)) {
+        		resultado.add(item);
+        	}
+         }
+        return(resultado);
+	}
 	public static void main(String[] args) {
 		BibliotecaDigital bib1 = new BibliotecaDigital();
 		
+		// adicionar
 		Livro livro1 = new Livro("Java Básico", "João Silva", 2015, 300);
 		Revista revista1 = new Revista("Tech News", "Editora Tech", 2020, 12);
 		bib1.adicionarItem(livro1);
@@ -38,9 +87,29 @@ public class BibliotecaDigital {
 		bib1.adicionarItem(new Livro("Inteligência Artificial: Uma Abordagem Moderna", "Stuart Russell e Peter Norvig", 2010, 1152));
 		bib1.adicionarItem(new Revista("Saúde & Bem-Estar", "Editora Viva", 2024, 1));
 		bib1.adicionarItem(new Revista("Automóvel Clássico", "Editora Rodas", 2017, 33));
-
-		//Listar
+		bib1.adicionarItem(new Revista("Automóvel Clássico", "Editora Rodas", 2018, 38));
+		
+		//Apagar
 		bib1.listarItens();
+		if (bib1.apagarItem("Java Básico"))
+			System.out.println("*** Item apagado ***");
+		else
+			System.out.println("*** Item nao encontrado ***");
+	
+		// testando caso de nao encontrar
+		if (bib1.apagarItem("Java Básico"))
+			System.out.println("*** Item apagado ***");
+		else
+			System.out.println("*** Item nao encontrado ***");
+		
+		//Listar por titulo
+		bib1.contarItensPorTipo();
+		
+		for (ItemBiblioteca item: bib1.buscarPorTitulo("Ciência Hoje")){
+			item.exibirInformacoes();
+		}
+		
+
 	}
 
 }
